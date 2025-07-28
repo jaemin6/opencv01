744eb263ff89aa655d7d5f16263e4575fbbfd02d
jaemin6 <gumt555@gmail.com>
2025-07-28 15:38:19 +0900

    이미지 + 텍스트

--- src/report.py
+++ src/report.py
@@ -1,21 +1,19 @@
 import cv2
 import numpy as np
 
-# 1. 흰 배경 이미지 만들기 + 텍스트 추가
-img = np.ones((300, 600, 3), dtype=np.uint8) * 255
-cv2.putText(img, "handsome guy", (100, 150),    
-            cv2.FONT_HERSHEY_SIMPLEX,       
-            2,                              
-            (255, 0, 255),                  
-            3,                              
-            cv2.LINE_AA)
-
-# 2. 이미지 불러와서 2배 확대
+# 1. 이미지 불러오기
 image = cv2.imread('../img/like_lenna.png')  # 이미지 경로 수정
 image_big = cv2.resize(image, dsize=None, fx=2, fy=2)
 
+# 2. 이미지 위에 텍스트 추가
+cv2.putText(image_big, "handsome guy", (50, 100),         # 위치(x, y)
+            cv2.FONT_HERSHEY_SIMPLEX,                     # 폰트 종류
+            2,                                            # 글자 크기
+            (255, 0, 255),                                # 색 (보라색)
+            3,                                            # 두께
+            cv2.LINE_AA)                                  # 선 형태
 
-# 5. 보여주기
-cv2.imshow("Text + Zoomed Image", combined)
+# 3. 보여주기
+cv2.imshow("Image with Text", image_big)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
