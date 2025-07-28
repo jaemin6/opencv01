c420b56c18fb3b0bc137e80a4e1b2f25b9c1f32e
jaemin6 <gumt555@gmail.com>
2025-07-28 13:04:01 +0900

    image resize

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,25 +1,29 @@
 import cv2
 import numpy as np
 
+# 이미지 불러오기
+image = cv2.imread('../img/like_lenna.png')
+
 new_height = 300
 new_width = 300
 
+# 이미지 리사이즈
+resized_image = cv2.resize(image, (new_width, new_height))
+
+# 이미지 보여주기
+cv2.imshow('Original Image', image)
+cv2.imshow('Resized Image', resized_image)
+
 dst = np.zeros((new_height, new_width), dtype=np.uint8)
+dst = cv2.resize(image, (new_width, new_height))
+
+#cv2.resize(image, (new_width, new_height), dst=dst)
 
 
-image = cv2.imread('../img/like_lenna.png')
-#image_small = cv2.resize(image, (100, 100))
-
-cv2.resize(image, (new_width, new_height), dst=dst)
-
-
-# 이미지를 보여주는 명령어
-cv2.imshow('Image Window', image)
-cv2.imshow('Image Window', dst)
-#cv2.imshow('Image Window', image_small)
 print(image.shape)
+print(resized_image.shape)
 
 cv2.waitKey(0)
-cv2.destoryAllWindow()
+cv2.destroyAllWindows()
 #cv2.imshow(image)
 
