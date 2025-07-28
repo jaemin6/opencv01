3bc66f9a8d23496fbbfd5f9bd2a1ad22ce08a71d
jaemin6 <gumt555@gmail.com>
2025-07-28 13:09:32 +0900

    상하 반전 대칭 변환

--- src/helloworld.py
+++ src/helloworld.py
@@ -7,16 +7,20 @@ image = cv2.imread('../img/like_lenna.png')
 new_height = 300
 new_width = 300
 
+# 상하 반전
+image_fliped = cv2.flip(image, 0)
+
 # 이미지 리사이즈
 resized_image = cv2.resize(image, (new_width, new_height))
 
 # 2. 2배 확대 (비율로 리사이즈)
-image_big = cv2.resize(image, dsize=None, fx=2, fy=2)
+#image_big = cv2.resize(image, dsize=None, fx=2, fy=2)
 
 # 이미지 보여주기
 cv2.imshow('Original Image', image)
 cv2.imshow('Resized Image (300x300)', resized_image)
-cv2.imshow('Zoomed x2 Image', image_big)
+cv2.imshow('Flipped Image (UpDown)', image_fliped)
+#cv2.imshow('Zoomed x2 Image', image_big)
 
 dst = np.zeros((new_height, new_width), dtype=np.uint8)
 dst = cv2.resize(image, (new_width, new_height))
@@ -26,6 +30,7 @@ dst = cv2.resize(image, (new_width, new_height))
 
 print(image.shape)
 print(resized_image.shape)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
